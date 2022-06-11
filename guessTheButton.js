47c3be23deccac2bab26db4e9d005924dcd35163
Rogojang77 <81257311+Rogojang77@users.noreply.github.com>
2022-06-11 20:19:29 +0300

    Delete guessTheButton.js

--- guessTheButton.js
+++
@@ -1,8 +0,0 @@
-let randomValue = Math.floor((Math.random() * 3) + 1); 
-function winnerButton(id) {
-  if (id == randomValue) {
-    alert("You find it!")
-  } else {
-    alert("Try again!")
-  }
-}
